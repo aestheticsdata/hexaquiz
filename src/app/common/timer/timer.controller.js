f94b6eee7bb5f624bf8736ff920ec84a415eb344
aestheticsdata <aesthetics.data@gmail.com>
2017-02-09 17:28:42 +0100

    timer service added

--- src/app/common/timer/timer.controller.js
+++ src/app/common/timer/timer.controller.js
@@ -1,45 +1,21 @@
-function TimerController($interval, $log, hlg) {
-    var ctrl=this,
-        count = 5, // number of seconds
-        tempMin = Math.floor(count / 60) % 60,
-        tempSec = count%60,
-        timer   = null;
+function TimerController($interval, $log, hlg, TimerService) {
+    var ctrl = this,
+        ctrlTimer = null;
 
-    _displayingTime();
 
     ctrl.$onInit = function () {
         $log.debug('TimerController');
-        // timer = $interval(_updateTimer, 1000);
+
+        updateTimerFromService();
+
+        TimerService.getTimerUpdate();
+
+        ctrlTimer = $interval(updateTimerFromService, 400);
     };
 
-    ctrl.$onChanges = function (changes) {
-        $log.debug('changes in timer : ', changes);
-        if (changes.launch.currentValue) {
-            timer = $interval(_updateTimer, 1000);
-        }
-    };
-
-    function _updateTimer() {
-        tempMin = Math.floor(count / 60) % 60;
-        tempSec = count%60;
-
-        count -= 1;
-
-        if (tempMin < 0 && tempSec < 0) {
-            ctrl.min = '00';
-            ctrl.sec = '00';
-            if($interval.cancel(timer)) {
-                window.alert('time elapsed');
-            }
-
-        } else {
-            _displayingTime();
-        }
-
-    }
-    function _displayingTime() {
-        ctrl.min = (tempMin < 10) ? '0' + tempMin : tempMin;
-        ctrl.sec = (tempSec < 10) ? '0' + tempSec : tempSec;
+    function updateTimerFromService() {
+        ctrl.min = TimerService.displayTime().min;
+        ctrl.sec = TimerService.displayTime().sec;
     }
 }
 
---
+++ src/app/common/timer/timer.service.js
@@ -0,0 +1,57 @@
+function TimerService($interval, $window, $log, hlg) {
+    var timerservice = {
+        count   : 5, // number of seconds
+        tempMin : 0,
+        tempSec : 0,
+        timer   : null,
+        getTimerUpdate : _getTimerUpdate,
+        updateTimer    : _updateTimer,
+        displayTime    : _displayTime
+    };
+
+    timerservice.tempMin = Math.floor(timerservice.count / 60) % 60;
+    timerservice.tempSec = timerservice.count%60;
+
+    return timerservice;
+
+
+    function _getTimerUpdate() {
+        if (!timerservice.timer) {
+            timerservice.timer = $interval(timerservice.updateTimer, 1000);
+        } else {
+
+        }
+    }
+
+    function _updateTimer() {
+        timerservice.tempMin = Math.floor(timerservice.count / 60) % 60;
+        timerservice.tempSec = timerservice.count%60;
+
+
+        if (timerservice.tempMin < 0 && timerservice.tempSec < 0) {
+            if($interval.cancel(timerservice.timer)) {
+                $window.alert('time elapsed');
+            }
+            timerservice.tempMin = '0';
+            timerservice.tempSec = '0';
+
+        } else {
+            timerservice.displayTime();
+        }
+
+        timerservice.count -= 1;
+
+    }
+
+    function _displayTime() {
+        var display = {
+            min : (timerservice.tempMin < 10) ? '0' + timerservice.tempMin : timerservice.tempMin,
+            sec : (timerservice.tempSec < 10) ? '0' + timerservice.tempSec : timerservice.tempSec
+        };
+        return display;
+    }
+}
+
+angular
+    .module('hexaquiz.common.timer')
+    .factory('TimerService', TimerService);
