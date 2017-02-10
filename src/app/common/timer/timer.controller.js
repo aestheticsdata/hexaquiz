1ae5379ac533e649a5e284ad216fc3af2c6784a2
aestheticsdata <aesthetics.data@gmail.com>
2017-02-10 19:49:55 +0100

    timer turned red when 0 is reached

--- src/app/common/timer/timer.controller.js
+++ src/app/common/timer/timer.controller.js
@@ -6,6 +6,8 @@ function TimerController($interval, $log, hlg, TimerService) {
     ctrl.$onInit = function () {
         $log.debug('TimerController');
 
+        ctrl.hasReachZero = false;
+
         updateTimerFromService();
 
         TimerService.getTimerUpdate();
@@ -21,6 +23,10 @@ function TimerController($interval, $log, hlg, TimerService) {
     function updateTimerFromService() {
         ctrl.min = TimerService.displayTime().min;
         ctrl.sec = TimerService.displayTime().sec;
+        if(ctrl.min == '00' && ctrl.sec == '00') {
+            $log.debug('has reach zero');
+            ctrl.hasReachZero = true;
+        }
     }
 }
 
