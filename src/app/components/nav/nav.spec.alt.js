40c1a42a621dfe6d8cac880011e5076cb990e2e6
aestheticsdata <aesthetics.data@gmail.com>
2017-03-15 15:16:06 +0100

    added alternate version of nav.spec with createSpy

--- src/app/components/nav/nav.spec.alt.js
+++ src/app/components/nav/nav.spec.alt.js
@@ -1,3 +1,47 @@
-/**
- * Created by cosmokaat on 15/03/17.
- */
+///////////////////////////////////////////////////////////////////
+// alternate version of nav.spec.js
+// this one uses jasmine.createSpy instead of spyOn an angular.noop
+///////////////////////////////////////////////////////////////////
+
+
+// describe('Nav component', function () {
+//     var $componentController,
+//         mockNavClick,
+//         controller;
+//
+//     beforeEach(function () {
+//         module('hexaquiz');
+//
+//         inject(function ($injector) {
+//             $componentController = $injector.get('$componentController');
+//             mockNavClick = jasmine.createSpy('onNavClick');
+//             controller = $componentController('nav',
+//                 null,
+//                 {onNavClick: mockNavClick}
+//             );
+//             controller.$onInit();
+//         });
+//     });
+//
+//     describe('Nav controller', function () {
+//         it('should have a prev method', function () {
+//             expect(controller.prev).toBeDefined();
+//         });
+//
+//         it('should have a next method', function () {
+//             expect(controller.next).toBeDefined();
+//         });
+//
+//         it('should call onNavClick with "prev" args', function () {
+//             controller.prev();
+//
+//             expect(mockNavClick).toHaveBeenCalledWith({$event:{dir:'prev'}});
+//         });
+//
+//         it('should call onNavClick with "next" args', function () {
+//             controller.next();
+//
+//             expect(mockNavClick).toHaveBeenCalledWith({$event:{dir:'next'}});
+//         });
+//     });
+// });
