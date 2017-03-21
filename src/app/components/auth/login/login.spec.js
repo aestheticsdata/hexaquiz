c102bdac5cd63594ec9b2d41c4779434eedbb26a
aestheticsdata <aesthetics.data@gmail.com>
2017-03-21 18:09:16 +0100

    unit test - login - work in progress

--- src/app/components/auth/login/login.spec.js
+++ src/app/components/auth/login/login.spec.js
@@ -1,42 +1,63 @@
 describe('Login', function () {
+    var $rootScope,
+        AuthService,
+        $q,
+        $componentController,
+        controller;
+
     beforeEach(module('hexaquiz'));
 
-    describe('Routes', function () {
-        var $state, $location, $rootScope;
+    beforeEach(inject(function ($injector) {
+        $state      = $injector.get('$state');
+        $location   = $injector.get('$location');
+        $rootScope  = $injector.get('$rootScope');
+        $q          = $injector.get('$q');
 
-        function goTo(url) {
-            $location.url(url);
-            $rootScope.$digest();
-        }
+        AuthService = $injector.get('AuthService');
+        $componentController = $injector.get('$componentController');
+        controller = $componentController('login',
+            { $scope: {}, AuthService: AuthService });
+        controller.$onInit();
+    }));
 
-        beforeEach(inject(function ($injector) {
-            $state = $injector.get('$state');
-            $location = $injector.get('$location');
-            $rootScope = $injector.get('$rootScope');
-        }));
-
-        // it('should redirect to auth.login state', function() {
-        //     goTo('/auth');
-        //     expect($state.current.name).toEqual('auth.login');
-        // });
-
-        it('should go to login state', function() {
-            goTo('/login');
-            expect($state.current.name).toEqual('login');
-        });
-    });
+    // describe('Routes', function () {
+    //     var $state, $location, $rootScope;
+    //
+    //     function goTo(url) {
+    //         $location.url(url);
+    //         $rootScope.$digest();
+    //     }
+    //
+    //
+    //
+    //     // it('should redirect to auth.login state', function() {
+    //     //     goTo('/auth');
+    //     //     expect($state.current.name).toEqual('auth.login');
+    //     // });
+    //
+    //     it('should go to login state', function() {
+    //         goTo('/login');
+    //         expect($state.current.name).toEqual('login');
+    //     });
+    // });
 
     describe('Controller', function () {
-        var $componentController, controller;
-
-        beforeEach(inject(function ($injector) {
-            $componentController = $injector.get('$componentController');
-            controller = $componentController('login', null);
-            controller.$onInit();
-        }));
 
         it('should call AuthService.login on loginUser()', function () {
-            spyOn(controller, 'loginUser');
+            var promise,
+                mockUser = {email: 'joe@cool.com', password: 'snoopy'},
+                mockEvent = {$event:{user: mockUser}};
+
+            // spyOn(controller, 'loginUser');
+            spyOn(AuthService, 'login').and.callFake(function () {
+                return $q.when({});
+            });
+
+            promise = controller.loginUser(mockEvent);
+            promise.then(function () {
+                expect(AuthService.login).toHaveBeenCalledWith(mockEvent.user);
+            });
+
         });
     });
 });
