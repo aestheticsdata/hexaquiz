16b8139ab3cf04d08d161f0f33d8751a5a556a24
aestheticsdata <aesthetics.data@gmail.com>
2017-03-29 18:06:42 +0200

    unit test - timer refactoring

--- src/app/common/timer/timer.spec.js
+++ src/app/common/timer/timer.spec.js
@@ -1,31 +1,46 @@
-describe('Timer Service', function () {
+describe('Timer', function () {
     var ts;
 
-    beforeEach(function () {
-        module('hexaquiz');
-    });
 
-    beforeEach(function () {
-        inject(function ($injector) {
-            ts = $injector.get('TimerService');
+    describe('Service', function () {
+        beforeEach(function () {
+            module('hexaquiz');
+        });
+
+        beforeEach(function () {
+            inject(function ($injector) {
+                ts = $injector.get('TimerService');
+            });
+        });
+
+
+        it('TimerService Should exist', function () {
+            expect(ts).toBeDefined();
+        });
+
+        it('has a count var', function () {
+            expect(ts.count).toBeDefined();
+        });
+
+        it('has a tempMin var', function () {
+            expect(ts.tempMin).toBeDefined();
+        });
+
+        it('has a tempSec var', function () {
+            expect(ts.tempSec).toBeDefined();
         });
     });
 
-
-    it('TimerService Should exist', function () {
-        expect(ts).toBeDefined();
-    });
-
-    it('has a count var', function () {
-        expect(ts.count).toBeDefined();
-    });
-
-    it('has a tempMin var', function () {
-        expect(ts.tempMin).toBeDefined();
-    });
-
-    it('has a tempSec var', function () {
-        expect(ts.tempSec).toBeDefined();
-    });
-
+    // describe('Controller', function () {
+    //     var $interval,
+    //         $componentController,
+    //         controller;
+    //
+    //     beforeEach(inject(function ($injector) {
+    //         $interval = $injector.get('$interval');
+    //         $componentController = $injector.get('$componentController');
+    //         controller = $componentController('timer', null);
+    //         controller.$onInit();
+    //     }));
+    // });
 });
